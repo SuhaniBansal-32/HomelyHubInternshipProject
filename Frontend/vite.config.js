bf640ab50319f91c85a39c8771920de0157aa123
SuhaniBansal-32 <suhanibansal32@gmail.com>
2026-09-24 17:01:11 +0530

    add deployed backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: "https://homelyhubinternshipproject-6flh.onrender.com",
         changeOrigin: true,
         secure: false,
       }
